0658829eb649083fdf2c1aa6904d9f118659ef70
KatanaYuki123 <83858536+KatanaYuki123@users.noreply.github.com>
2024-07-22 01:40:36 +0700

    Signed build kek

--- device.mk
+++ device.mk
@@ -450,6 +450,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Signed Build
+$(call inherit-product, vendor/extra/product.mk)
+
 # Telephony
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
