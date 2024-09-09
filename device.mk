ed81ba332e88fecede04fc7f8662711eb6bc8209
cecilia8964 <83858536+cecilia8964@users.noreply.github.com>
2024-09-09 19:47:47 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -385,6 +385,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_k7tn/android.hardware.se.omapi.ese.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_k7tn/com.android.nfc_extras.xml
 
+# Platform
+TARGET_BOARD_PLATFORM := bengal
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
